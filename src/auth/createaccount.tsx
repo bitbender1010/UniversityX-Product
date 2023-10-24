763c2b28c0d0506beca22e20f21b77ae88167dc7
bitbender1010 <bitbender1010@gmail.com>
2023-10-24 03:07:09 +0100

    aaa

--- src/auth/createaccount.tsx
+++ src/auth/createaccount.tsx
@@ -1,61 +1,29 @@
 
-import Logo from '../images/UniversityX-Logo.png'
-import AuthImage from '../images/auth-image.png'
-// import GoogleImg from '../images/google-icon.svg'
 import '../css/auth.css'
 
+const CreateAccount = () => {
 
-function CreateAccount() {
 
-    // const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    //     event.preventDefault()
-    //     const data = new FormData(event.currentTarget)
-    //     fetch('/', {
-    //         method: 'POST',
-    //         body: data,
-    //         headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    //     })
-    //         .then(() => alert('Form submitted successfully!'))
-    //         .catch((error) => alert(error))
-    // }
+    return (
+        <form
+        name="contact"
+        method="POST"
+        data-netlify="true"
+        netlify-honeypot="test-field"
+      >
+        <input type="hidden" name="contact" value="contact" />
+            <p>
+                <label>Your Name: <input type="text" name="name" /></label>
+            </p>
+            <p>
+                <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+                <button type="submit">Send</button>
+            </p>
+            </form>
 
-    return(
-        <>
-            <div className='auth-parent'>
-                <div className='auth-left'>
-                <a href={`/`}> <div className='ca-logo'> <img src={Logo}/> </div> </a>
+    );
+};
 
-                    <div className='auth-left-inner'>
-                        <div className='cre-acc-txt font-medium'>Create an account</div>
-                        <div className='disclaimer'><span>Disclaimer:</span> UniversityX is currently available to only a few selected Schools</div>
-
-                        {/* Netlify Form */}
-                        <form name="create-account" method="POST" data-netlify="true">
-                            <input type="hidden" name="form-name" value="create-account" />
-                            <div>
-                                <label htmlFor="name">Name:</label>
-                                <input type="text" id="name" name="name" required />
-                            </div>
-                            <div>
-                                <label htmlFor="email">Email:</label>
-                                <input type="email" id="email" name="email" required />
-                            </div>
-                            <div>
-                                <label htmlFor="password">Password:</label>
-                                <input type="password" id="password" name="password" required />
-                            </div>
-                            <button type="submit">Create Account</button>
-                        </form>
-                        {/* End Netlify Form */}
-                    </div>
-                </div>
-
-                <div className='auth-right'>
-                    <img src={AuthImage}/>
-                </div>
-            </div>
-        </>
-    )
-}
-
-export default CreateAccount
+export default CreateAccount;
