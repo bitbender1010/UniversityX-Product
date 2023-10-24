fcaf81846cb6842c0a98dec25a87ecbf8ba64b82
bitbender1010 <bitbender1010@gmail.com>
2023-10-24 03:14:26 +0100

    as

--- src/auth/createaccount.tsx
+++ src/auth/createaccount.tsx
@@ -5,23 +5,20 @@ const CreateAccount = () => {
 
 
     return (
-        <form
-        name="contact"
-        method="POST"
-        data-netlify="true"
-        netlify-honeypot="test-field"
-      >
-        <input type="hidden" name="contact" value="contact" />
-            <p>
-                <label>Your Name: <input type="text" name="name" /></label>
-            </p>
-            <p>
-                <label>Your Email: <input type="email" name="email" /></label>
-            </p>
-            <p>
-                <button type="submit">Send</button>
-            </p>
-            </form>
+        <form name="contact" method="POST" data-netlify="true">
+        
+        <input type="hidden" name="form-name" value="contact" />
+
+        <input type="text" placeholder="Name" name="name" required />
+        <input type="text" placeholder="Email" name="email" required />
+
+
+        <button
+          type="submit"
+        >
+          Send
+        </button>
+      </form>
 
     );
 };
